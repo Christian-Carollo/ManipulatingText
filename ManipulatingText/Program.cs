474083180e1c6513746b839699425ded8edbcc6f
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-09 19:44:43 +0100

    Aggiungere i file di progetto.

---
+++ ManipulatingText/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class ManipulatingText
+{
+    static void Main()
+    {
+        Console.WriteLine("Inserisci del testo:");
+        string input = Console.ReadLine();
+
+        string reversedText = ReverseText(input);
+
+        Console.WriteLine("Testo al contrario: " + reversedText);
+    }
+
+    static string ReverseText(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
